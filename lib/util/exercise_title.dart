ca97727a1b7c842bb4bd35aa6e43d87693728d8b
NavaweeV5 <6310210720@psu.ac.th>
2023-07-23 00:36:07 +0700

    practice flutter clone

---
+++ lib/util/exercise_title.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTitlePage extends StatelessWidget {
+  final iconexercise;
+  final String exerciseName;
+  final String numberOfExercises;
+  final color;
+
+  const ExerciseTitlePage({
+    Key? key, // Add the 'key' parameter here
+    required this.iconexercise,
+    required this.exerciseName,
+    required this.numberOfExercises,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(16)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    color: color,
+                    child: Icon(
+                      iconexercise,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 14,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //title
+                    Text(
+                      exerciseName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    //subtitle
+                    Text(
+                      numberOfExercises.toString() + ' Exercises',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Icon(Icons.more_horiz),
+          ],
+        ),
+      ),
+    );
+  }
+}
